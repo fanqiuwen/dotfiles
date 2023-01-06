65d52187531e89bd24b0509d45bff178b515f08f
Owen Fan <fanqiuwen@gmail.com>
2023-01-05 17:37:16 -0800

    Make Google-specific changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,8 @@
 if status is-interactive
-  # Symlink ssh-agent socket to a static location for tmux. If not using a
-  # forwarded ssh-agent, use gpg-agent as ssh-agent.
-  set -l ssh_auth_sock "$HOME/.ssh/ssh_auth_sock"
-  if test "$SSH_AUTH_SOCK" != $ssh_auth_sock
-    if test -n "$SSH_AUTH_SOCK"
-      ln -sf $SSH_AUTH_SOCK $ssh_auth_sock
-      set -x SSH_AUTH_SOCK $ssh_auth_sock
-    else if command -q gpg
-      ln -sf (gpgconf --list-dirs agent-ssh-socket) $ssh_auth_sock
-      set -x SSH_AUTH_SOCK $ssh_auth_sock
-    end
-  end
-
   # If on ssh and not in tmux, start tmux.
   if test -n "$SSH_CLIENT" -a -z "$TMUX" && command -q tmux
-    exec tmux new-session -A -s ssh
+    exec tmx2 new-session -A -s ssh
   end
+
+  source_google_fish_package hi
 end
